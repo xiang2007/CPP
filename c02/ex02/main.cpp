449de830737514344d1694dad82f79f5a77e0690
shou xiang <weeshouxiang@gmail.com>
2026-06-11 17:19:05 +0800

    fix: - removed comments from main file - Removed executable

--- c02/ex02/main.cpp
+++ c02/ex02/main.cpp
@@ -4,20 +4,6 @@
 using std::cout;
 using std::endl;
 
-// int main( void ) 
-// {
-// 	Fixed a;
-// 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-// 	std::cout << a << std::endl;
-// 	std::cout << ++a << std::endl;
-// 	std::cout << a << std::endl;
-// 	std::cout << a++ << std::endl;
-// 	std::cout << a << std::endl;
-// 	std::cout << b << std::endl;
-// 	std::cout << Fixed::max( a, b ) << std::endl;
-// 	return 0;
-// }
-
 int main()
 {
 	{
